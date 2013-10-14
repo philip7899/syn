eee255711d61c3271366250d4e004ffc9aff96e1
Philip Stitzer <pghrpg@gmail.com>
2013-10-14 13:28:53 -0400

    Static Pages created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syn::Application.config.secret_key_base = '2f3ce90abdc3b76143bbf7008d09ae8e7f627003315b1fe58e82fd6df83b59fd7aa06b18c0bba7d984a37a916cc3c8c4cc37f6971d48c265ad08a4f8f7cbab49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Syn::Application.config.session_store :cookie_store, key: '_syn_session'
